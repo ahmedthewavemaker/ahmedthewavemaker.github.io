7366893acf377d72b622b717bb6ed3d172def17a
ahmedthewavemaker <72584721+ahmedthewavemaker@users.noreply.github.com>
2020-11-25 20:10:05 -0500

    2nd endpoint api commit
    results display functional

--- script.js
+++ script.js
@@ -27,7 +27,68 @@ function getLink(input) {
 
     fetch(url)
         .then(response => response.json())
-        .then(repsponseJson => console.log(repsponseJson))
+        .then(responseJson => displayResults(responseJson))
+        .catch(error => {
+            console.error('Something is wrong!');
+            console.error(error)
+        });
+    }
+
+function getDetails(venueId){
+    const params = {
+        client_id: clientId,
+        client_secret: clientSecret,
+        v: '20201120'
+    
+    };
+    const queryString = formatQueryParams(params)
+    const url = `https://api.foursquare.com/v2/venues/${venueId}` + '?' + queryString;
+    console.log(url);
+
+    fetch(url)
+        .then(response => response.json())
+        .then(responseJson => displayDetails(responseJson))
+        .catch(error => {
+            console.error('Something is wrong!');
+            console.error(error)
+        });
+}
+
+function displayDetails(responseJson){
+    console.log(responseJson);
+    $('#details').html(`
+    ${responseJson.response.venue.name}<br>
+    ${responseJson.response.venue.bestPhoto}
+    ${responseJson.response.venue.url}
+    `)
+
+}
+
+
+
+//displays results of the search
+function displayResults(responseJson) {
+    console.log(responseJson);
+    $('#results').empty();
+
+    for (let i = 0; i < responseJson.response.venues.length; i++) {
+        $('#results').append(`<li><h3><button onClick="getDetails('${responseJson.response.venues[i].id}')">
+        ${responseJson.response.venues[i].name}</button></h3></li>`)
+        console.log(responseJson.response.venues[i].url);
+    }
+
+    $('#js-display, #back-button').removeClass('hidden');
+    $('#home, #js-form').addClass('hidden');
+
+}
+
+function homeButton() {
+    $('#back-button').click(e => {
+        e.preventDefault();
+
+        $('#js-display, #back-button').addClass('hidden');
+        $('#home, #js-form').removeClass('hidden');
+    })
 }
 
 
@@ -37,10 +98,11 @@ function watchForm() {
     $('#js-form').submit(e => {
         e.preventDefault();
         let input = $('#city-state').val();
+        e.target.reset();
         getLink(input);
-        
-    })
 
+    })
+    homeButton();
 
 }
 
